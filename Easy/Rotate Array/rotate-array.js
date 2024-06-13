268e14845e03c01e8edaf9b1c9e742a9d70daaf0
AKSHAY . C . S <147317339+akshaycsasidharan@users.noreply.github.com>
2024-06-13 19:29:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotate Array/rotate-array.js
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar0 = readLine().split(' ').map(x=>parseInt(x));
+        let n = input_ar0[0];
+        let d = input_ar0[1];
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        obj.rotateArr(arr, d, n);
+        let S = '';
+        for(let i=0;i<n;i++)
+        {
+            S+=arr[i];
+            S+=' ';
+        }
+        console.log(S);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} d
+ * @param {number} n
+ */
+ 
+
+
+
+class Solution {
+    rotateArr(arr, d, n) {
+        d = d % n; 
+
+
+        function reverse(arr, start, end) {
+            while (start < end) {
+                [arr[start], arr[end]] = [arr[end], arr[start]];
+                start++;
+                end--;
+            }
+        }
+
+
+        reverse(arr, 0, d - 1);
+
+
+        reverse(arr, d, n - 1);
+
+
+        reverse(arr, 0, n - 1);
+
+        return arr;
+    }
+}
